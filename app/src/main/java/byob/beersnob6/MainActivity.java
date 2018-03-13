dbbb3affb5efcd38a846dddd2031878c3f409e66
Carissa Kane <carissakane@Carissas-MacBook-Pro.local>
2018-03-13 15:21:56 -0700

    Restock page updated with link to Amazon.

--- app/src/main/java/byob/beersnob6/MainActivity.java
+++ app/src/main/java/byob/beersnob6/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
--- app/src/main/java/byob/beersnob6/RestockBeerActivity.java
+++ app/src/main/java/byob/beersnob6/RestockBeerActivity.java
@@ -1,17 +1,56 @@
 package byob.beersnob6;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 /**
- * Created by Nikka Mofid on 1/11/2018.
+ * Restock Beer Activity
  */
 
-public class RestockBeerActivity extends Activity {
+public class RestockBeerActivity extends AppCompatActivity {
+    private final String url = "http://www.amazon.com/fresh";
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.restockbeer);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        String title = "Beer Snob Restock Page";
+        SpannableString s = new SpannableString(title);
+        s.setSpan(new ForegroundColorSpan(Color.BLACK), 0, title.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        getSupportActionBar().setTitle(s);
+
+        TextView report = (TextView)findViewById(R.id.textView);
+        report.setTextColor(Color.WHITE);
+        report.setText("Resock Beer at Amazon's Store");
+
+        android.widget.Button Restock;
+        Restock = (android.widget.Button)findViewById(R.id.button9);
+        Restock.setTextSize(20);
+        Restock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Log.d("Onclick","Im Alive!");
+                Uri uri = Uri.parse(url);
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                (RestockBeerActivity.this).startActivity(intent);
+
+            }
+
+        });
 
     }
 }
